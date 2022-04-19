b897664474dd12b4393a32b2829b2262624edf02
Joaquin <Joaquin@LAPTOP-IHSB4S9N>
2022-04-18 21:25:51 -0500

    Add project files.

---
+++ ClaseS3/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ClaseS3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            string user = txtUser.Text;
+            string pass = txtContrasena.Text;
+
+            await Navigation.PushAsync(new pagina_dos(user, pass));
+
+
+        }
+
+
+        private void buttonPaginados_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ClaseS3/pagina_dos.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ClaseS3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class pagina_dos : ContentPage
+    {
+        public pagina_dos(string user, string pass)
+        {
+            InitializeComponent();
+            DisplayAlert("Mensaje", "El usuario es" + user + "La contrasena es" + pass, "cerrar");
+        }
+    }
+}
